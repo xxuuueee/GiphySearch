8700ad3b6816ff3735d2e509e1bed96e5c0ae02d
Xue (Shay) Wu <xuemin2505@gmail.com>
2017-06-18 01:04:48 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,82 @@
+/* 1. Grab the input*/
+var button = document.querySelector(".js-go");
+
+
+
+button.addEventListener("click", function(){
+	var input=document.querySelector("input").value;
+	
+	createInput(input);
+
+	pushToDOM(input);
+})
+
+document.querySelector(".js-userinput").addEventListener('keyup',function(e){
+
+  var input = document.querySelector("input").value;
+  
+
+  // if the key ENTER is pressed...
+  if(e.which === 13) {
+    createInput(input);
+    pushToDOM(input);
+  }
+
+});
+
+
+
+/*2. dO THE DATA STUFF WITH THE API*/
+function createInput(input){
+	var url = "http://api.giphy.com/v1/gifs/search?q=\"" + input + "\"&api_key=dc6zaTOxFJmzC";
+
+
+// AJAX Request
+	var GiphyAJAXCall = new XMLHttpRequest();
+	GiphyAJAXCall.open( 'GET', url );
+	GiphyAJAXCall.send();
+
+
+	GiphyAJAXCall.addEventListener('load', function( e ) {
+		var data = e.target.response; /* From the load function you want to drill down the contentyou want to grab, in this case the GIFS*/
+	
+		pushToDOM(data);
+		//console.log(data);
+
+
+})};
+
+/*3. SHOW ME THE GIFS*/
+function pushToDOM(input){
+	
+	var response = JSON.parse(input);
+	console.log(response);
+
+	var imageUrls = response.data;
+	//console.log(imageUrls);
+	var container = document.querySelector(".js-container");
+	container.innerHTML = " ";
+
+
+	imageUrls.forEach(function(image){
+		var src =image.images.fixed_height.url;
+		//console.log(src);
+
+		container.innerHTML += "<img src = \"" + src + "\"  class = \"container-image \">";
+		
+
+	
+
+		
+
+		});
+
+	
+
+	
+
+
+	
+
+
+}
